b5a98b656e326847cd10736ca36bfa4c18c620c2
Vlad Petrenko <52258477+Pe1renko@users.noreply.github.com>
2021-10-02 19:28:38 +0700

    upd readme

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
+var img = document.querySelector('img');
+
+img.onclick = () => {
+    var src = img.getAttribute('src');
+    
+    if (src == `images/chelik.jpg`) {
+        img.setAttribute(`src`, `images/chelik2.jpg`);
+    } else {
+        img.setAttribute(`src`, "images/chelik.jpg");
+    };
+};
