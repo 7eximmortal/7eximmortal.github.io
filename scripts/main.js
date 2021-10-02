5f4b1c6225362a436a3136b1188718cc5666a511
Vlad Petrenko <52258477+Pe1renko@users.noreply.github.com>
2021-10-02 19:36:52 +0700

    upd site

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ var img = document.querySelector('img');
 
 img.onclick = () => {
     var src = img.getAttribute('src');
-    
+
     if (src == `images/chelik.jpg`) {
-        img.setAttribute(`src`, `images/chelik2.jpg`);
+        img.setAttribute(`src`, `images/chelik3.jpg`);
     } else {
         img.setAttribute(`src`, "images/chelik.jpg");
     };
