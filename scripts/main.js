d83da80ba04d1c9450174573a0f34afc78e54c98
Vlad Petrenko <52258477+Pe1renko@users.noreply.github.com>
2021-10-02 22:49:00 +0700

    upd site sanya

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
 var button = document.querySelector(`button`);
 var heading = document.querySelector(`h1`);
+var img = document.querySelector(`img`);
 
 function setUserName() {
     var name = prompt(`Please enter your name.`)
     localStorage.setItem(`name`, name);
     heading.textContent = `${name}`;
+    if(/саня/i.test(heading.textContent)) {
+        img.setAttribute(`src`, `images/sanya.jpg`)
+    } else {
+        img.setAttribute(`src`, `images/masha.jpg`)
+    }
 };
 
 if(!localStorage.getItem(`name`)) {
